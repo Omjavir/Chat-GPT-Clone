bf2b533837dd6fd5a099c28df6952ee92c5d9047
Om Suresh Javir <javirom07042003@gmail.com>
2022-12-29 23:49:11 +0530

    Final commit

--- client/script.js
+++ client/script.js
@@ -86,7 +86,9 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codex-im0y.onrender.com/', {
+        // Uncomment below line for development
+        // const response = await fetch('http://localhost:5000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
